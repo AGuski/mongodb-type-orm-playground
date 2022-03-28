ccd40c01dc46f7619f5506d37f0a8babe6104b91
Glitch (glitch-hello-node) <none>
2022-03-28 16:28:32 +0000

    🐠🍂 Checkpoint ./tsconfig.json:19970270/48 ./src/server.ts:19970270/107

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,12 @@ import {createConnection} from "typeorm";
 
 console.log('starting ...');
 
-const connection = await createConnection({
+const connection = createConnection({
     type: "mongodb",
     host: "cluster0.33pph.mongodb.net",
-    port: 3306,
+    port: 27015,
     username: "thestoreroomguy",
     password: "d8ZU1zj1aSL7Vnd5",
     database: "myFirstDatabase"
-});
+}).then(connection => console.log(connection), error => console.log(error));
+
